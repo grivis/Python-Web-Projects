450893b9164d711f9ac4cf5da7a5c1302fab7c49
grigory <sss>
2016-10-23 12:28:39 +0300

    Котировки ЦБ РФ в  виде словаря

--- CBR-Daily-Quotes.py
+++ CBR-Daily-Quotes.py
@@ -19,19 +19,31 @@ regTDClose = re.compile('</td>', flags=re.U | re.DOTALL)
 regTRopen = re.compile('<tr>', flags=re.U | re.DOTALL)
 regTRClose = re.compile('</tr>', flags=re.U | re.DOTALL)
 regTag = re.compile('<.*?>', flags=re.U | re.DOTALL)
-#regSpace = re.compile('\s{2,}', flags=re.U | re.DOTALL)
 
 for t in titles:
     t = t.replace('\r', '')
-    t = t.replace('\n', ' ')
+    t = t.replace('\n', '')
     clean_t = regTDopen.sub('', t)
-    clean_t = regTDClose.sub('', clean_t)
+    clean_t = regTDClose.sub('#', clean_t)
     clean_t = regTRopen.sub('', clean_t)
     clean_t = regTRClose.sub('*', clean_t)
     clean_t = regTag.sub("", clean_t)
-    #clean_t = regSpace.sub("*", clean_t)
     currlst = clean_t.split('*')
 
-
+allquotes = []
 for currency in currlst:
     print(currency)
+    clst = currency.split('#')
+    allquotes.append(clst)
+
+allquotes = allquotes[1:-1]
+
+quotdic = {}
+#{'XYZ':(Имя валюты, кратность, курс, цифровой код)}
+
+for i in allquotes:
+    quotdic[i[1]] = (i[3] , int(i[2]) , float(i[4].replace(',', '.')), i[0])
+
+print(quotdic)
+
+
