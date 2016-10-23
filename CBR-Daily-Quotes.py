bc27485f40c52c188f6d7f9e829043bb76d5c86f
grigory <sss>
2016-10-23 12:43:50 +0300

    Котировки ЦБ РФ в  виде словаря

--- CBR-Daily-Quotes.py
+++ CBR-Daily-Quotes.py
@@ -1,49 +1,55 @@
-import urllib.request  # импортируем модуль
-import re
-
-url = 'http://cbr.ru/currency_base/daily.aspx?date_req=24.10.2016'  # адрес страницы, которую мы хотим скачать
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'  # хотим притворяться браузером
-
-req = urllib.request.Request(url, headers={'User-Agent': user_agent})
-# добавили в запрос информацию о том, что мы браузер Мозилла
-
-with urllib.request.urlopen(req) as response:
-    html = response.read().decode('utf-8')
-
-regPostTitle = re.compile('<table class="data">.*?</table>', flags=re.U | re.DOTALL)
-titles = regPostTitle.findall(html)
 
 
-regTDopen = re.compile('<td>', flags=re.U | re.DOTALL)
-regTDClose = re.compile('</td>', flags=re.U | re.DOTALL)
-regTRopen = re.compile('<tr>', flags=re.U | re.DOTALL)
-regTRClose = re.compile('</tr>', flags=re.U | re.DOTALL)
-regTag = re.compile('<.*?>', flags=re.U | re.DOTALL)
+def cbquotes(dd, mm, yyyy):
+    import urllib.request  # импортируем модуль
+    import re
 
-for t in titles:
-    t = t.replace('\r', '')
-    t = t.replace('\n', '')
-    clean_t = regTDopen.sub('', t)
-    clean_t = regTDClose.sub('#', clean_t)
-    clean_t = regTRopen.sub('', clean_t)
-    clean_t = regTRClose.sub('*', clean_t)
-    clean_t = regTag.sub("", clean_t)
-    currlst = clean_t.split('*')
+    urlbase = 'http://cbr.ru/currency_base/daily.aspx?date_req='  # базовый адрес страницы котировок
+    url = urlbase + dd +'.' + mm + '.' + yyyy
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'  # хотим притворяться браузером
 
-allquotes = []
-for currency in currlst:
-    print(currency)
-    clst = currency.split('#')
-    allquotes.append(clst)
+    req = urllib.request.Request(url, headers={'User-Agent': user_agent})
+    # добавили в запрос информацию о том, что мы браузер Мозилла
 
-allquotes = allquotes[1:-1]
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
 
-quotdic = {}
-#{'XYZ':(Имя валюты, кратность, курс, цифровой код)}
+    regPostTitle = re.compile('<table class="data">.*?</table>', flags=re.U | re.DOTALL)
+    titles = regPostTitle.findall(html)
 
-for i in allquotes:
-    quotdic[i[1]] = (i[3] , int(i[2]) , float(i[4].replace(',', '.')), i[0])
 
-print(quotdic)
+    regTDopen = re.compile('<td>', flags=re.U | re.DOTALL)
+    regTDClose = re.compile('</td>', flags=re.U | re.DOTALL)
+    regTRopen = re.compile('<tr>', flags=re.U | re.DOTALL)
+    regTRClose = re.compile('</tr>', flags=re.U | re.DOTALL)
+    regTag = re.compile('<.*?>', flags=re.U | re.DOTALL)
+
+    for t in titles:
+        t = t.replace('\r', '')
+        t = t.replace('\n', '')
+        clean_t = regTDopen.sub('', t)
+        clean_t = regTDClose.sub('#', clean_t)
+        clean_t = regTRopen.sub('', clean_t)
+        clean_t = regTRClose.sub('*', clean_t)
+        clean_t = regTag.sub("", clean_t)
+        currlst = clean_t.split('*')
+
+    allquotes = []
+    for currency in currlst:
+        clst = currency.split('#')
+        allquotes.append(clst)
+
+    allquotes = allquotes[1:-1]
+
+    quotdic = {}
+    #{'XYZ':(Имя валюты, кратность, курс, цифровой код)}
+
+    for i in allquotes:
+        quotdic[i[1]] = (i[3] , int(i[2]) , float(i[4].replace(',', '.')), i[0])
+
+
+    return quotdic
+
+
 
 
