d252f85cc42492c04eb15c02f27f50a0049baa0e
grigory <sss>
2016-10-30 12:33:05 +0300

    Скачивание таблицы с помощью BS

--- BS-Test.py
+++ BS-Test.py
@@ -2,8 +2,25 @@
 import urllib.request  # импортируем модуль
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen('http://cbr.ru/currency_base/daily.aspx?date_req=25.10.2016')
+import re
+
+html = urlopen('http://cbr.ru/currency_base/daily.aspx?date_req=30.10.2016')
 bsobj = BeautifulSoup(html.read())
-print(bsobj.h1)
+#print(bsobj.h2.get_text())
+#print(bsobj.table.get_text())
+quotes = []
 for i in bsobj.table.tr.next_siblings:
-    print(i)
+    il = str(i)
+    quotes.append(il)
+
+for i in range(len(quotes)):
+    try:
+        quotes.remove('\n')
+    except ValueError:
+        break
+
+regTag = re.compile('<.*?>', flags=re.U | re.DOTALL)
+for item in quotes:
+    clean_item = regTag.sub("", item)
+    clean_item_lst = clean_item.split('\n')
+    print(clean_item_lst)
